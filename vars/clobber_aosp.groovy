7e15b7de2102f2c23d800b013939e6f92fdd76ac
Qais Yousef <qyousef@layalina.io>
2022-01-04 12:05:27 +0000

    clobber_aosp: Only do clean builds if we are told too
    Weird errors occurs that require restarting the build. By default don't
    clean but give the user the option to force clean builds if they want
    to.
    
    Signed-off-by: Qais Yousef <qyousef@layalina.io>

--- vars/clobber_aosp.groovy
+++ vars/clobber_aosp.groovy
@@ -1,11 +1,13 @@
-def call() {
+def call(clean_build) {
 	sh """
-		if [ -d aosp ]; then
-			pushd aosp
-			source build/envsetup.sh
-			m clean
-			repo forall -c git clean -xdf
-			popd
+		if [ "${clean_build}" == "true" ]; then
+			if [ -d aosp ]; then
+				pushd aosp
+				source build/envsetup.sh
+				m clean
+				repo forall -c git clean -xdf
+				popd
+			fi
 		fi
 	"""
 }
