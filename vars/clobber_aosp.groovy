4d793f2346fe64cab3a2408b25dd858bf1f1530b
Qais Yousef <qyousef@layalina.io>
2022-01-24 10:37:08 +0000

    builder-aosp: Make clean stage conditional
    Signed-off-by: Qais Yousef <qyousef@layalina.io>

--- vars/clobber_aosp.groovy
+++ vars/clobber_aosp.groovy
@@ -1,13 +1,11 @@
-def call(clean_build) {
+def call() {
 	sh """
-		if [ "${clean_build}" == "true" ]; then
-			if [ -d aosp ]; then
-				pushd aosp
-				source build/envsetup.sh
-				m clean
-				repo forall -c git clean -xdf
-				popd
-			fi
+		if [ -d aosp ]; then
+			pushd aosp
+			source build/envsetup.sh
+			m clean
+			repo forall -c git clean -xdf
+			popd
 		fi
 	"""
 }
