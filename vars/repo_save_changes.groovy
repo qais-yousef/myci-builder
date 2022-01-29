fe409598a76aeadd5d356ccb7337f526ca3ea867
Qais Yousef <qyousef@layalina.io>
2022-01-29 14:05:56 +0000

    repo_save_changes: handle variations in how BRANCH/TAG is used
    Android-kernels have a different format for the tag. Try to handle that.
    
    Signed-off-by: Qais Yousef <qyousef@layalina.io>

--- vars/repo_save_changes.groovy
+++ vars/repo_save_changes.groovy
@@ -3,6 +3,7 @@ def call(repo_dir, branch) {
 		pushd ${repo_dir}
 
 		repo forall -c "git format-patch --suffix=\\"--myci.patch\\" ${branch}..HEAD" || true
+		repo forall -c "git format-patch --suffix=\\"--myci.patch\\" m/${branch}..HEAD" || true
 
 		popd
 	"""
