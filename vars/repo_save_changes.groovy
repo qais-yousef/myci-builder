a138deb2d3465e670af8b5bf45351e166d343a66
Qais Yousef <qyousef@layalina.io>
2022-01-29 00:55:43 +0000

    repo_save_changes: We can use simpler call
    As Jenkins already saves the directory path for us, duh!
    
    And allow empty archives for this rule.
    
    Signed-off-by: Qais Yousef <qyousef@layalina.io>

--- vars/repo_save_changes.groovy
+++ vars/repo_save_changes.groovy
@@ -2,7 +2,7 @@ def call(branch) {
 	sh """
 		pushd aosp
 
-		repo forall -c "git format-patch --suffix=\\"--myci--\\\$(echo \\\$REPO_PROJECT | sed 's#/#_#g').patch\\" ${branch}..HEAD" || true
+		repo forall -c "git format-patch --suffix=\\"--myci.patch\\" ${branch}..HEAD" || true
 
 		popd
 	"""
