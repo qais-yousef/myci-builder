5c21d1a5c95e12d64dcc4ee69230a871f41ca7a9
Qais Yousef <qyousef@layalina.io>
2022-02-14 17:47:35 +0000

    build-android-kernel: Filter out mixed and test build scripts
    Some kernels provide multiple build scripts. Filter out known ones that
    we don't want. Not bullet proof but there's no guaranteees what will the
    script name be across branches.
    
    Signed-off-by: Qais Yousef <qyousef@layalina.io>

--- vars/compile_android_kernel.groovy
+++ vars/compile_android_kernel.groovy
@@ -2,7 +2,7 @@ def call() {
 	sh """
 		pushd android-kernel
 
-		build_script=`ls build*.sh`
+		build_script=`ls build*.sh | grep -vE '(mixed|test)'`
 
 		if [ "x\$build_script" == "x" ]; then
 			./build/build.sh
