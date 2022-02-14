355516bf80939ce6a24e4483698d6dc4eedc9d2f
Qais Yousef <qyousef@layalina.io>
2022-02-14 18:09:36 +0000

    compile_android_kernel: Don't use prebuilts
    We must export BUILD_KERNEL=1 to avoid using prebuilts automatically,
    which seems to be the default setup on latest android devices.
    
    Signed-off-by: Qais Yousef <qyousef@layalina.io>

--- vars/compile_android_kernel.groovy
+++ vars/compile_android_kernel.groovy
@@ -4,6 +4,9 @@ def call() {
 
 		build_script=`ls build*.sh | grep -vE '(mixed|test)'`
 
+		# In age of GKI, this is required to not use prebuilts
+		export BUILD_KERNEL=1
+
 		if [ "x\$build_script" == "x" ]; then
 			./build/build.sh
 		else
