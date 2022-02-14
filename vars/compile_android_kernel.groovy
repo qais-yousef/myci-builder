cef89c7fad4bbb9d8395c77d5db95fa78c12d74c
Qais Yousef <qyousef@layalina.io>
2022-02-14 16:30:41 +0000

    compile_android_kernel: Use any provided build_script if present
    As some kernels require additional steps that are usually wrapped by
    that top-level script.
    
    Signed-off-by: Qais Yousef <qyousef@layalina.io>

--- vars/compile_android_kernel.groovy
+++ vars/compile_android_kernel.groovy
@@ -2,7 +2,13 @@ def call() {
 	sh """
 		pushd android-kernel
 
-		./build/build.sh
+		build_script=`ls build*.sh`
+
+		if [ "x\$build_script" == "x" ]; then
+			./build/build.sh
+		else
+			./\$build_script
+		fi
 
 		popd
 	"""
