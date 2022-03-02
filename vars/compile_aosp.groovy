d8a9001ea73f098e0dcd8a6da4285691eda5b911
Qais Yousef <qyousef@layalina.io>
2022-03-02 21:25:45 +0000

    builder-aosp: Support building UiBench and JankBench apks.
    Signed-off-by: Qais Yousef <qyousef@layalina.io>

--- vars/compile_aosp.groovy
+++ vars/compile_aosp.groovy
@@ -1,10 +1,22 @@
-def call(target, num_parallel_jobs) {
+def call(target, num_parallel_jobs, uibench=false, jankbench=false) {
 	sh """
 		pushd aosp
 
 		source build/envsetup.sh
 		lunch ${target}
 		m -j ${num_parallel_jobs}
+
+		if [ "${uibench}" == "true" ]; then
+			pushd frameworks/base/tests/UiBench
+			mm -j ${num_parallel_jobs}
+			popd
+		fi
+
+		if [ "${jankbench}" == "true" ]; then
+			pushd frameworks/base/tests/JankBench
+			mm -j ${num_parallel_jobs}
+			popd
+		fi
 
 		popd
 	"""
